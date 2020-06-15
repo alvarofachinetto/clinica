812ce56b237689a8e93feaf689d99b0df01891fb
Alvaro Silva <alvaro.facneto@gmail.com>
2020-06-14 22:57:53 -0300

    Config eureka in medicos

--- medicos/src/main/java/com/clinica/medicos/MedicosApplication.java
+++ medicos/src/main/java/com/clinica/medicos/MedicosApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import com.clinica.medicos.model.Especialidade;
 import com.clinica.medicos.model.Medico;
@@ -16,6 +17,7 @@ import com.clinica.medicos.repository.MedicoRepository;
 import net.bytebuddy.utility.RandomString;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MedicosApplication implements CommandLineRunner{
 
 	@Autowired
